0568d7c42593adf676ffca623324e339cc3aa809
Almadina <Almadina@DESKTOP-HDKHLI0>
2024-01-02 10:47:34 -0800

    Add project files.

---
+++ Api task/Server/DbContext.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace Api task.Server
+{
+    public class DbContext
+{
+    public DbContext(DbContextOptions<DbContext> options) : base(options) { }
+
+    public DbSet<DbContext> YourModels { get; set; }
+}
+}
+// Startup.cs
+public void ConfigureServices(IServiceCollection services)
+{
+    // ...
+
+    services.AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "Your API", Version = "v1" });
+    });
+}
+
+public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+{
+    // ...
+
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Your API V1");
+    });
+}
+
---
+++ Api task/Server/Program.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.ResponseCompression;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+    // Startup.cs
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddDbContext<DbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+        // Add other services as needed
+    }
+
+}
+else
+{
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapRazorPages();
+app.MapControllers();
+app.MapFallbackToFile("index.html");
+
+app.Run();
